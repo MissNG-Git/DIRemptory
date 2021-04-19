b72d38f9f499e03ad065b32c5938e2f5af0c56fd
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-04-19 17:02:02 +1000

    personalize footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,31 @@
 import React from "react";
+import "../../index.css";
 
 function Footer() {
   return (
-    <div className="footer">
-      <p>I'm a footer!</p>
-    </div>
+    <footer className="footer text-muted">
+      <a href="https://github.com/MissNG-Git" className="footer-ref">
+        <i className="fab fa-github fa-2x"></i>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/jessie-n-9302b962/"
+        className="footer-ref"
+      >
+        <i className="fab fa-linkedin fa-2x"></i>
+      </a>{" "}
+      <br />
+      <span>
+        Made with love by{" "}
+        <strong>
+          {" "}
+          <em className="handle">MissNG</em>
+        </strong>{" "}
+        © 2021
+        <br />
+        Employee data acquired from{" "}
+        <a href="https://randomuser.me/">Randomuser.Me</a>
+      </span>
+    </footer>
   );
 }
 
