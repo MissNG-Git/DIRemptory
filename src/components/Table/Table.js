292f46333263557d701d5bcbe198bae3595cff46
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-04-25 02:46:12 +1000

    structure table & code column headings

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,11 +1,32 @@
 import React from "react";
 import Row from "../Row/Row";
 
-function Table() {
+function Table(props) {
   return (
     <table className="table">
-      <p>The table will hold employee data in Rows!</p>
-      <Row />
+      <thead>
+        <tr>
+          <th scope="col">Image</th>
+          {/* data-field = column field name */}
+          {/* data-sortable = allows column sorting */}
+          <th scope="col" data-field="column header" data-sortable="true">
+            <span>Name</span>
+          </th>
+          <th scope="col">
+            <span>Phone Number</span>
+          </th>
+          <th scope="col">
+            <span>Email Address</span>
+          </th>
+          <th scope="col">
+            <span>Date of Birth</span>
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <Row />
+      </tbody>
     </table>
   );
 }
