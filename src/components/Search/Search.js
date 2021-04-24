3bd8d0ed194449ecd47ed764dc32bd2ef1b9d64f
MissNG <65439386+MissNG-Git@users.noreply.github.com>
2021-04-25 02:24:26 +1000

    restructure search bar for inputs to go through filter code

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,65 +1,23 @@
 import React from "react";
 import "./Search.css";
-// import DropdownButton from "react-bootstrap/DropdownButton";
-// import DropdownItem from "react-bootstrap/esm/DropdownItem";
 
-export default function Search({
-  handleNameSearch,
-  handleMaleFilter,
-  handleFemaleFilter,
-  filterGender,
-}) {
+function Search(props) {
   return (
     <div className="search-container">
-      <form className="search-form">
+      <form className="search-form" onSubmit={props.handleFormSubmit}>
         <span className="search-span">
           <input
             className="search-bar"
-            id="name-filter"
-            onChange={handleNameSearch}
-            placeholder="Search Employee Name"
-            type="text"
-          ></input>
-        </span>
-
-        {/* <DropdownButton
-          className="genderFilter"
-          id="dropdown-basic-button"
-          title="Filter by"
-        >
-          <DropdownItem>Male</DropdownItem>
-          <DropdownItem>Female</DropdownItem>
-        </DropdownButton> */}
-
-        <span className="filter-span">
-          Filter by:{"  "}
-          <input
-            className="genderFilter"
-            id="female-filter"
-            name="gender"
-            onChange={handleMaleFilter}
-            type="radio"
-            value="male"
-          ></input>{" "}
-          <label for="male"> Male</label>
-          <input
-            className="genderFilter"
-            id="female-filter"
-            name="gender"
-            onChange={handleFemaleFilter}
-            type="radio"
-            value="female"
-          ></input>{" "}
-          <label for="female"> Female</label> {"  "}
-          <button
-            className="genderFilter"
-            id="filter-btn"
-            onClick={filterGender}
-          >
-            <i className="fas fa-filter"></i>
-          </button>
+            name="search"
+            onChange={props.handleInputChange}
+            placeholder="Search"
+            type="search"
+            value={props.value}
+          />
         </span>
       </form>
     </div>
   );
 }
+
+export default Search;
